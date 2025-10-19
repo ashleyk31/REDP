b46f456c4f70b58ec6df96c3ac035174d39c1b64
Ashley Kang <142348207+ashleyk31@users.noreply.github.com>
2025-10-19 12:05:50 -0400

    Add files via upload

---
+++ htmlfile.py
@@ -0,0 +1,72 @@
+import pandas as pd
+pd.set_option("display.max_colwidth", None)
+df = pd.read_csv("../result_files/ballot_analysis.csv")
+
+# format data
+# print(df["debate"])
+# print(df.columns)
+conversation = df["debate"]
+split_list = conversation.iloc[0].split("<br><br>")
+df_split = pd.DataFrame([split_list])
+# print(df_split)
+
+df_transpose = df_split.T
+# print(df_transpose.shape)
+# print(df_transpose)
+
+# new dataframe for the conversation only
+df_conversation = pd.DataFrame({
+    "Voter 1 [FOR]": df_transpose.iloc[::2, 0].reset_index(drop=True),
+    "Voter 2 [AGAINST]": df_transpose.iloc[1::2, 0].reset_index(drop=True)
+})
+
+
+df_conversation["Voter 1 [FOR]"] = df_conversation["Voter 1 [FOR]"].str.split(": ", n=1).str[1]
+df_conversation["Voter 2 [AGAINST]"] = df_conversation["Voter 2 [AGAINST]"].str.split(": ", n=1).str[1]
+
+
+print(df_conversation)
+html_string = """
+<html>
+<head>
+<style>
+body { font-family: sans-serif, Ariel; }
+table { width: 80%; border-collapse: collapse; margin: 20px auto; }
+th { padding: 20px; text-align: center; font-size: 18px; }
+th.voter1 { text-align: left; }
+th.voter2 { text-align: right; }
+td { width: 50%; vertical-align: top; padding: 30px; word-wrap: break-word; }
+td.voter1 { background-color: #e6f7ff; text-align: left; border-radius: 15px; }
+td.voter2 { background-color: #e8ffc6; text-align: right; border-radius: 15px; }
+</style>
+</head>
+<body>
+<h2 style="text-align:center;">Background </h2>
+<h2 style="text-align:center;">Ballot Measure </h2>
+<h2 style="text-align:center;">What does a YES vote mean? </h2>
+<h2 style="text-align:center;">What does a NO vote mean? </h2>
+<h2 style="text-align:center;">Voter Debate</h2>
+<table>
+<tr><th class='voter1'>Voter 1 [FOR]</th><th class='voter2'>Voter 2 [AGAINST]</th></tr>
+"""
+
+# loop through the conversation
+for idx, row in df_conversation.iterrows():
+    # Voter 1
+    html_string += f"<tr><td class='voter1'>{row['Voter 1 [FOR]']}</td><td></td></tr>\n"
+
+    # Voter 2
+    html_string += f"<tr><td></td><td class='voter2'>{row['Voter 2 [AGAINST]']}</td></tr>\n"
+
+html_string += """
+</table>
+</body>
+</html>
+"""
+
+
+# write file
+with open("conversation.html", "w") as f:
+    f.write(html_string)
+
+print("HTML file created: conversation.html")
